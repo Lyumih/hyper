c49e0b7f10738bb2d5707f72dd41fa08ca9e5c01
Lyumih <Lyumih@users.noreply.github.com>
2023-10-10 07:13:08 +0000

    Deploying to gh-pages from @ Lyumih/hyper@17c32abc57883c0fc5ec01197fe714931c8715c3 🚀

--- web.js
+++ web.js
@@ -2755,6 +2755,15 @@ var $;
         dom_name() {
             return "a";
         }
+        uri_off() {
+            return "";
+        }
+        uri_native() {
+            return null;
+        }
+        external() {
+            return false;
+        }
         attr() {
             return {
                 ...super.attr(),
