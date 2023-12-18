90caf864fd85bf57971421b913117794be2e3a75
Lyumih <Lyumih@users.noreply.github.com>
2023-12-18 07:13:09 +0000

    Deploying to gh-pages from @ Lyumih/hyper@9dbbe6aadd81dc6dc2bc60d53c09f4721967f860 🚀

--- node.test.js
+++ node.test.js
@@ -3407,7 +3407,8 @@ var $;
                 title: this.hint_safe(),
                 target: this.target(),
                 download: this.file_name(),
-                mol_link_current: this.current()
+                mol_link_current: this.current(),
+                rel: this.relation()
             };
         }
         sub() {
@@ -3441,6 +3442,9 @@ var $;
         }
         current() {
             return false;
+        }
+        relation() {
+            return "";
         }
         event_click(event) {
             if (event !== undefined)
--- web.mjs
+++ web.mjs
@@ -2953,7 +2953,8 @@ var $;
                 title: this.hint_safe(),
                 target: this.target(),
                 download: this.file_name(),
-                mol_link_current: this.current()
+                mol_link_current: this.current(),
+                rel: this.relation()
             };
         }
         sub() {
@@ -2987,6 +2988,9 @@ var $;
         }
         current() {
             return false;
+        }
+        relation() {
+            return "";
         }
         event_click(event) {
             if (event !== undefined)
