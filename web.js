f1f7f9050a3c584363788f2685f65c71a8ecc5c8
Lyumih <Lyumih@users.noreply.github.com>
2024-11-23 07:14:50 +0000

    Deploying to gh-pages from @ Lyumih/hyper@9dbbe6aadd81dc6dc2bc60d53c09f4721967f860 🚀

--- web.js
+++ web.js
@@ -4753,8 +4753,8 @@ var $;
 		dom_name(){
 			return "article";
 		}
-		field(){
-			return {...(super.field()), "tabIndex": (this.tabindex())};
+		attr(){
+			return {...(super.attr()), "tabIndex": (this.tabindex())};
 		}
 		sub(){
 			return [
