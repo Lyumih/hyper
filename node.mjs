9095f894de7f24d24ea0097f3549c2e4c0f90b37
Lyumih <Lyumih@users.noreply.github.com>
2024-12-08 07:15:47 +0000

    Deploying to gh-pages from @ Lyumih/hyper@9dbbe6aadd81dc6dc2bc60d53c09f4721967f860 🚀

--- node.mjs
+++ node.mjs
@@ -6691,7 +6691,7 @@ var $;
             display: 'block',
             position: 'relative',
             font: {
-                family: 'monospace',
+                family: 'inherit',
             },
             Numb: {
                 textAlign: 'right',
--- web.mjs
+++ web.mjs
@@ -6117,7 +6117,7 @@ var $;
             display: 'block',
             position: 'relative',
             font: {
-                family: 'monospace',
+                family: 'inherit',
             },
             Numb: {
                 textAlign: 'right',
