1687e260f196c1d37f50c279adf1b6013c70696b
Lyumih <Lyumih@users.noreply.github.com>
2025-01-01 07:15:44 +0000

    Deploying to gh-pages from @ Lyumih/hyper@9dbbe6aadd81dc6dc2bc60d53c09f4721967f860 🚀

--- web.js
+++ web.js
@@ -5212,7 +5212,6 @@ var $;
             maxHeight: per(100),
             boxSizing: 'border-box',
             color: $mol_theme.text,
-            backdropFilter: blur(`3px`),
             ':focus': {
                 outline: 'none',
             },
