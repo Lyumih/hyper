3836ef918efe241a1b6735fbbdf3c27e67ccda6a
Lyumih <Lyumih@users.noreply.github.com>
2025-01-19 07:14:30 +0000

    Deploying to gh-pages from @ Lyumih/hyper@9dbbe6aadd81dc6dc2bc60d53c09f4721967f860 🚀

--- node.js
+++ node.js
@@ -3385,6 +3385,7 @@ var $;
         static go(next) {
             this.href(this.link(next));
         }
+        static commit() { }
         constructor(prefix = '') {
             super();
             this.prefix = prefix;
--- node.test.js
+++ node.test.js
@@ -3376,6 +3376,7 @@ var $;
         static go(next) {
             this.href(this.link(next));
         }
+        static commit() { }
         constructor(prefix = '') {
             super();
             this.prefix = prefix;
