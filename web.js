5569f617708b1d2448871f4d7ece49524f18bd54
Lyumih <Lyumih@users.noreply.github.com>
2025-11-23 07:17:52 +0000

    Deploying to gh-pages from @ Lyumih/hyper@9dbbe6aadd81dc6dc2bc60d53c09f4721967f860 🚀

--- web.js
+++ web.js
@@ -11077,8 +11077,9 @@ var $;
 		changed(){
 			return false;
 		}
-		state(){
-			return {};
+		state(next){
+			if(next !== undefined) return next;
+			return null;
 		}
 		state_pick(id, next){
 			if(next !== undefined) return next;
@@ -11127,6 +11128,7 @@ var $;
 	($mol_mem(($.$mol_form_draft.prototype), "Reset"));
 	($mol_mem(($.$mol_form_draft.prototype), "model"));
 	($mol_mem_key(($.$mol_form_draft.prototype), "model_pick"));
+	($mol_mem(($.$mol_form_draft.prototype), "state"));
 	($mol_mem_key(($.$mol_form_draft.prototype), "state_pick"));
 	($mol_mem_key(($.$mol_form_draft.prototype), "value"));
 	($mol_mem_key(($.$mol_form_draft.prototype), "value_str"));
@@ -11137,6 +11139,9 @@ var $;
 	($mol_mem(($.$mol_form_draft.prototype), "reset"));
 	($mol_mem(($.$mol_form_draft.prototype), "done"));
 
+
+;
+"use strict";
 
 ;
 "use strict";
